06e75c8771ac043bf41b34ed1ac2a081e7ac58a8
Dominik Mrówka <dmrowka7@gmail.com>
2022-07-02 14:34:44 +0200

    Add unit test for convertPLNToUSD

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,7 +1,17 @@
 export const convertPLNToUSD = (PLN) => {
 
+  if (typeof PLN === 'string' || PLN instanceof String) {
+    return NaN
+  } else if (PLN === undefined) {
+    return NaN
+  } else if (typeof PLN !== 'number' && typeof PLN !== 'string') {
+    return 'Error';
+  } else if (PLN < 0) {
+    return 0
+  };
+
   const PLNtoUSD = PLN / 3.5;
-  
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
